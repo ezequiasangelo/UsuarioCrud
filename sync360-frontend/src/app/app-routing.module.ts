82036dbbeb697ad3735d312fbd04cfcd3cd95cb6
ezequiasangelo <ezequias21666@hotmail.com>
2025-07-07 19:30:54 -0300

    feat: Implementa CRUD de usuário com upload de imagem via Cloudinary

---
+++ sync360-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UsuarioComponent } from './component/usuario/usuario.component';
+
+const routes: Routes = [
+  {
+    path:'', 
+    redirectTo:'usuario',
+    pathMatch:'full'
+  },
+  {
+    path:'usuario',
+    component:UsuarioComponent  
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ sync360-frontend/src/app/shared/service/usuario.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Usuario {
+  isDelete: any;
+  id?: number;
+  nome_completo: string;
+  idade?: number;
+  rua?: string;
+  bairro?: string;
+  estado?: string;
+  biografia?: string;
+  imagem_perfil?: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  private apiBaseUrl = 'http://127.0.0.1:8000/api';
+
+  constructor(private http: HttpClient) {}
+
+  getUsuarios(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(`${this.apiBaseUrl}/usuarios`);
+  }
+
+  getUsuario(id: number): Observable<Usuario> {
+    return this.http.get<Usuario>(`${this.apiBaseUrl}/usuario/${id}`);
+  }
+
+  createUsuario(usuario: Usuario): Observable<any> {
+    return this.http.post(`${this.apiBaseUrl}/usuario`, usuario);
+  }
+
+  updateUsuario(id: number, usuario: Usuario): Observable<any> {
+    return this.http.put(`${this.apiBaseUrl}/usuario/${id}`, usuario);
+  }
+
+  deleteUsuario(id: number): Observable<any> {
+    return this.http.delete(`${this.apiBaseUrl}/usuario/${id}`);
+  }
+
+  // Utilitário para salvar (criar ou atualizar automaticamente com base no ID)
+  saveUsuario(usuario: Usuario): Observable<any> {
+    if (usuario.id) {
+      return this.updateUsuario(usuario.id, usuario);
+    }
+    return this.createUsuario(usuario);
+  }
+}
